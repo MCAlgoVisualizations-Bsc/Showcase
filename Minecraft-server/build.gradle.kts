c97463d12df135e65a85e404c66532e25914d9ad
Bror Hansen <64813210+SneekySnek@users.noreply.github.com>
2026-01-22 18:03:06 +0100

    Initial setup of minestom localhost
    Initializes and creates an instance of a minecraft world, generates a chunk of land, adds basic lighting and a global eventhandler for spawning instances. When run it stats a port on localhost: 25565

--- Minecraft-server/build.gradle.kts
+++ Minecraft-server/build.gradle.kts
@@ -13,6 +13,8 @@ dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    implementation("net.minestom:minestom:2026.01.08-1.21.11") //Added Minstom dependency
+    implementation("ch.qos.logback:logback-classic:1.4.14") // Added logging, file saving and formatting
 }
 
 tasks.test {
--- Minecraft-server/src/main/java/org/example/Main.java
+++ Minecraft-server/src/main/java/org/example/Main.java
@@ -1,9 +1,40 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import net.minestom.server.MinecraftServer;
+import net.minestom.server.entity.Player;
+import net.minestom.server.event.GlobalEventHandler;
+import net.minestom.server.event.player.AsyncPlayerConfigurationEvent;
+import net.minestom.server.instance.*;
+import net.minestom.server.instance.block.Block;
+import net.minestom.server.coordinate.Pos;
+import net.minestom.server.utils.mojang.MojangUtils;
+
 public class Main {
-    static void main() {
-        IO.println(String.format("Hello and welcome!"));
-        }
+    public static void main(String[] args) {
+        // Initialization
+        MinecraftServer minecraftServer = MinecraftServer.init();
+
+        // Create the instance
+        InstanceManager instanceManager = MinecraftServer.getInstanceManager();
+        InstanceContainer instanceContainer = instanceManager.createInstanceContainer();
+
+        // Set the ChunkGenerator
+        instanceContainer.setGenerator(unit -> unit.modifier().fillHeight(0, 40, Block.GRASS_BLOCK));
+
+        //Add lighting
+        instanceContainer.setChunkSupplier(LightingChunk::new);
+
+        // Add an event callback to specify the spawning instance (and the spawn position)
+        GlobalEventHandler globalEventHandler = MinecraftServer.getGlobalEventHandler();
+        globalEventHandler.addListener(AsyncPlayerConfigurationEvent.class, event -> {
+            final Player player = event.getPlayer();
+            event.setSpawningInstance(instanceContainer);
+            player.setRespawnPoint(new Pos(0, 42, 0));
+        });
+
+
+
+        // Start the server on port 25565
+        minecraftServer.start("0.0.0.0", 25565);
     }
+}
