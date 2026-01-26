999e0d3e01cb706ae8f4c3b92194b2bd5a28272d
André Birk <arbi@itu.dk>
2026-01-26 14:27:05 +0100

    added teleport and refactored to kotlin

--- Minecraft-server/build.gradle.kts
+++ Minecraft-server/build.gradle.kts
@@ -1,7 +1,15 @@
 plugins {
     java
     id("com.gradleup.shadow") version "9.3.0" //shadowjar
+    kotlin("jvm") version "2.3.0"
 }
+// 1. Force Java Toolchain to 21 (This handles 'compileJava')
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(25))
+    }
+}
+
 
 group = "org.example"
 version = "1.0-SNAPSHOT"
@@ -11,15 +19,24 @@ repositories {
 }
 
 dependencies {
+
+    // Add the Kotlin Standard Library
+    implementation(kotlin("stdlib"))
+
     // Your Testing Dependencies
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
-    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    //testImplementation("org.junit.jupiter:junit-jupiter")
+    //testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 
     // Your Server Dependencies
     implementation("net.minestom:minestom:2026.01.08-1.21.11")
     implementation("ch.qos.logback:logback-classic:1.4.14")
+    implementation("io.github.revxrsal:lamp.common:4.0.0-rc.14")
+    implementation("io.github.revxrsal:lamp.minestom:4.0.0-rc.14")
+
 }
+
+
 
 tasks {
     test {
