1b7b84802d478f4b97b5b22f84d5458e87c88512
Lukas <lukasvranic@gmail.com>
2026-02-09 12:08:02 +0100

    added javadoc to DisplayValue

--- lib/src/main/java/io/github/mcalgovisualizations/visualization/DisplayValue.java
+++ lib/src/main/java/io/github/mcalgovisualizations/visualization/DisplayValue.java
@@ -14,13 +14,28 @@ import net.minestom.server.instance.Instance;
 import net.minestom.server.instance.block.Block;
 import org.jspecify.annotations.NonNull;
 
+/**
+ * Represents a visual element in a visualization, consisting of a 3D block
+ * and an associated floating text label.
+ *
+ * @param <T> The type of the value being visualized, must be {@link Comparable}.
+ */
 public class DisplayValue<T extends Comparable<T>> implements Comparable<DisplayValue<T>> {
     private final T value;
     private final Entity entity;
     private final Entity text;
 
+    /**
+     * Constructs a new DisplayValue and spawns it into the specified instance.
+     *
+     * @param instance The Minestom instance where the entities will be spawned.
+     * @param value    The data value associated with this display.
+     * @param block    The block type to be used for the visual representation.
+     */
     public DisplayValue(Instance instance, T value, Block block) {
         this.value = value;
+
+        // Setup Block Display
         entity = new Entity(EntityType.BLOCK_DISPLAY);
         BlockDisplayMeta blockMeta = (BlockDisplayMeta) entity.getEntityMeta();
         blockMeta.setBlockState(block);
@@ -31,7 +46,7 @@ public class DisplayValue<T extends Comparable<T>> implements Comparable<Display
         blockMeta.setTransformationInterpolationStartDelta(0);
         blockMeta.setScale(new Vec(2.0, 2.0, 2));
 
-        // Setup Text
+        // Setup Text Display
         text = new Entity(EntityType.TEXT_DISPLAY);
         TextDisplayMeta textMeta = (TextDisplayMeta) text.getEntityMeta();
         textMeta.setText(Component.text(value.toString(), NamedTextColor.GOLD));
@@ -45,29 +60,57 @@ public class DisplayValue<T extends Comparable<T>> implements Comparable<Display
         text.setInstance(instance);
     }
 
+    /**
+     * Compares this DisplayValue with another based on their underlying data values.
+     *
+     * @param o The other DisplayValue to compare against.
+     * @return A negative integer, zero, or a positive integer as this value
+     * is less than, equal to, or greater than the specified object.
+     */
     @Override
     public int compareTo(@NonNull DisplayValue<T> o) {
         return this.value.compareTo(o.value);
     }
 
+    /**
+     * @return The underlying data value.
+     */
     public T getValue() {
         return value;
     }
 
+    /**
+     * Removes both the block and text entities from their current instance.
+     */
     public void remove() {
         entity.remove();
         text.remove();
     }
 
+    /**
+     * Checks if the entities are currently associated with an instance.
+     * * @return true if both entities are currently despawned/unassigned.
+     */
     public boolean isSpawned() {
         return entity.getInstance() == null && text.getInstance() == null;
     }
 
+    /**
+     * Teleports the block to the specified position and offsets the text label
+     * to remain centered above the block.
+     *
+     * @param pos The new base position for the block display.
+     */
     public void teleport(Pos pos) {
         entity.teleport(pos);
         text.teleport(pos.add(1, 2.4, 1));
     }
 
+    /**
+     * Toggles the glowing effect (highlight) on both the block and text entities.
+     *
+     * @param highlighted true to enable glowing, false to disable.
+     */
     public void setHighlighted(boolean highlighted) {
         entity.setGlowing(highlighted);
         text.setGlowing(highlighted);
