8e57a7821f65c67f4e3fe4f81b1cd786da8cd616
Bror Hansen <64813210+SneekySnek@users.noreply.github.com>
2026-02-09 12:03:40 +0100

    Update build.gradle.kts to work work with docker

--- server/build.gradle.kts
+++ server/build.gradle.kts
@@ -1,8 +1,15 @@
 plugins {
     id("java")
+    id("com.gradleup.shadow") version "9.3.0"
 }
 
-
+sourceSets {
+    main {
+        java {
+            srcDirs("src")
+        }
+    }
+}
 
 group = "io.github.mcalgovisualizations"
 version = "unspecified"
@@ -12,13 +19,33 @@ repositories {
 }
 
 dependencies {
+    // Server Dependencies
     implementation("net.minestom:minestom:2026.01.08-1.21.11")
+    implementation("ch.qos.logback:logback-classic:1.5.13")
+
+    // Project Dependency
     implementation(project(":lib"))
+
+    // Testing Dependencies
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
-tasks.test {
-    useJUnitPlatform()
+tasks {
+    test {
+        useJUnitPlatform()
+    }
+
+    shadowJar {
+        manifest {
+            attributes["Main-Class"] = "Main"
+        }
+        archiveBaseName.set("minecraft-server")
+        archiveClassifier.set("all")
+    }
+
+    build {
+        dependsOn(shadowJar)
+    }
 }
